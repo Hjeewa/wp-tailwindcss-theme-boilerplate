1c8829907a2b5d4c8def105aae988abaad807d10
Huzaifah Jeewa <huzaifahjeewa@gmail.com>
2019-12-09 11:03:10 +0000

    removed the example js imports

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,5 @@
 // you can import modules from the theme lib or even from
 // NPM packages if they support it…
-import ExampleComponent1 from "./components/ExampleComponent1";
-
-// you can also require modules if they support it…
-const ExampleModule2 = require('./components/example-2');
 
 // Some convenient tools to get you started…
 import ReplaceObfuscatedEmailAddresses from "./components/ReplaceObfuscatedEmailAddresses";
