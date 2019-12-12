7885ceb7931b11c0b3d5620f82661edd6baf7ec8
Huzaifah Jeewa <huzaifahjeewa@gmail.com>
2019-12-12 19:33:43 +0000

    post content template

---
+++ templates/loop/content.php
@@ -0,0 +1,39 @@
+<?php
+
+// Post rendering content according to caller of get_template_part.
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+?>
+
+<article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
+
+	<header class="post-header">
+
+		<?php the_title(sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ),'</a></h2>');?>
+
+		<?php if ( 'post' == get_post_type() ) : ?>
+
+			<div class="entry-meta">
+				<?php vlTailwind_entry_cats();?>
+			</div><!-- .entry-meta -->
+
+		<?php endif; ?>
+
+	</header><!-- .entry-header -->
+
+	<?php echo get_the_post_thumbnail( $post->ID, 'large' ); ?>
+
+	<div class="post-content">
+
+		<?php the_excerpt(); ?>
+
+	</div><!-- .entry-content -->
+
+	<footer class="post-footer">
+		<?php vlTailwind_posted_on(); ?>
+		<?php vlTailwind_entry_footer(); ?>
+
+	</footer><!-- .entry-footer -->
+
+</article><!-- #post-## -->
