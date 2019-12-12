b32df27320e9f7f1fa3ab228ee9b9af6f59f429c
Huzaifah Jeewa <huzaifahjeewa@gmail.com>
2019-12-12 21:20:05 +0000

    fixed the search fields ID to render results

--- searchform.php
+++ searchform.php
@@ -4,9 +4,9 @@ defined( 'ABSPATH' ) || exit;
 ?>
 
 <form method="get" id="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>" role="search">
-	<label class="sr-only" for="search_field">Search</label>
+	<label class="sr-only" for="s">Search</label>
 	<div>
-		<input id="search_field" class="" name="search" type="text" placeholder="<?php esc_attr_e( 'Search &hellip;'); ?>" value="<?php the_search_query(); ?>">
+		<input id="s" class="" name="s" type="text" placeholder="<?php esc_attr_e( 'Search &hellip;'); ?>" value="<?php the_search_query(); ?>">
 		<button id="search_submit" class="" name="submit" type="submit">Search</button>
 	</div>
 </form>
