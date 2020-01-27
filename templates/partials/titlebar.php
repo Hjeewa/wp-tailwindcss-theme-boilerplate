08950aef4372b3880acc4713667de85d5bbccfbb
Huzaifah Jeewa <info@vergelabs.co.uk>
2020-01-27 22:23:45 +0000

    titlebar conditionals. local and global options

--- templates/partials/titlebar.php
+++ templates/partials/titlebar.php
@@ -1,19 +1,38 @@
 <?php 
 
 
-$title = false;
-$breadcrumbs = false;
+if(get_field('show_page_title_local') != 'global' ){
+	$show_page_title = get_field('show_page_title_local');
+}
+else{
+	$show_page_title = get_field('show_page_title_global','option');
+};
+
+
+if(get_field('show_breadcrumbs_local') != 'global' ){
+	$show_breadcrumbs = get_field('show_breadcrumbs_local');
+}
+else{
+	$show_breadcrumbs = get_field('show_breadcrumbs_global','option');
+};
+
+$page_subtitle =  get_field( 'page_subtitle' );
 
 ?>
 
+<?php if ($show_page_title == 'show' || $show_breadcrumbs == 'show' ): ?>
+
 <div>
     <div class="container">
-        <?php if ( function_exists( 'vlTailwind_title' ) && ($title === true)): ?>
+
+        <?php if ($show_page_title == 'show'): ?>
             <h1><?php echo vlTailwind_title();?></h1>
         <?php endif;?>
 
-        <?php if ( function_exists( 'vlTailwind_breadcrumb_trail' ) && ($breadcrumbs === true)): ?>
+        <?php if ($show_breadcrumbs == 'show'): ?>
             <?php vlTailwind_breadcrumb_trail();?>
         <?php endif;?>
     </div>
 </div>
+
+<?php endif;?>
