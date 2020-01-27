72bc90889e2d309cbda7f662208928e0bb5e5a90
Huzaifah Jeewa <info@vergelabs.co.uk>
2020-01-27 20:25:50 +0000

    added acf options page

--- includes/acf.php
+++ includes/acf.php
@@ -8,3 +8,15 @@ add_filter( 'acf/settings/load_json', function ( $paths ) {
     $paths[] = get_template_directory().'/acf-json';
     return $paths;
 });
+
+
+if( function_exists('acf_add_options_page') ) {
+	
+	acf_add_options_page(array(
+		'page_title' 	=> 'Theme Settings',
+		'menu_title'	=> 'Theme Settings',
+		'menu_slug' 	=> 'theme-settings',
+		'capability'	=> 'edit_posts',
+		'redirect'		=> false
+	));
+}
