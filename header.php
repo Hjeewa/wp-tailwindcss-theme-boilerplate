0099f0f71a028c7a20cd38407aec7bca1210910d
Huzaifah Jeewa <info@vergelabs.co.uk>
2020-01-28 13:16:58 +0000

    added header template

--- header.php
+++ header.php
@@ -1,5 +1,17 @@
 <?php include( locate_template( 'templates/partials/document-open.php', false, false ) );?>
 
-<?php include( locate_template( 'templates/partials/titlebar.php', false, false ) );?>
+<?php 
 
+
+?>
+
+<header class="pt-5">
+    <div>
+        <div class="container"></div>
+    </div>
+
+    <?php include( locate_template( 'templates/partials/titlebar.php', false, false ) );?>
+
+
+</header>
 <!-- Site header markup goes here -->
